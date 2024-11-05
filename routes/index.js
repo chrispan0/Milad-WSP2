531f2a4e9f7eb0768c43d7fb43cd275ba1409dfa
MiladAhmadi10 <milad.ahmadi@ontariotechu.net>
2024-11-05 15:06:53 -0500

    renamed index.ejs, added route and fixed source

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,4 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-router.get("/about", (req, res) => {
-  res.render("about", { title: "About Me" });
-});
-
-router.get("/projects", (req, res) => {
-  res.render("projects", { title: "Projects" });
-});
-
-router.get("/contact", (req, res) => {
-  res.render("contact", { title: "Contact" });
-});
-
-router.get("/github", (req, res) => {
-  res.render("github", { title: "GitHub" });
-});
-
 module.exports = router;
