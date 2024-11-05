bc532d164419a3dbfa45aa93dd8dfd0b264e24bf
chrispan-i3 <github@chrispanetta.com>
2024-11-05 18:48:58 -0500

    added docker files needed for milad

---
+++ mkdocker.bat
@@ -0,0 +1,7 @@
+git fetch
+git pull
+docker stop Milad-WSP2
+docker rm Milad-WSP2
+cd C:\Users\Administrator\Documents\Milad\
+docker build -t Milad-WSP2 .
+docker run -d -p 1337:4000 --name Milad-WSP2 Milad-WSP2:latest
