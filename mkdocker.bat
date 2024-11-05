fef6fdb8bc7d8c8138f9f80d66589b2b345e2c37
chrispan-i3 <github@chrispanetta.com>
2024-11-05 18:49:31 -0500

    changed port

--- mkdocker.bat
+++ mkdocker.bat
@@ -4,4 +4,4 @@ docker stop Milad-WSP2
 docker rm Milad-WSP2
 cd C:\Users\Administrator\Documents\Milad\
 docker build -t Milad-WSP2 .
-docker run -d -p 1337:4000 --name Milad-WSP2 Milad-WSP2:latest
+docker run -d -p 2222:4000 --name Milad-WSP2 Milad-WSP2:latest
