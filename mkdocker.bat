2fc477d10e2dc0683841da0a3fe8d460c2869534
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 19:03:07 -0500

    fixed

--- mkdocker.bat
+++ mkdocker.bat
@@ -2,7 +2,7 @@ git fetch
 git pull
 docker stop milad-wsp2
 docker rm milad-wsp2
-cd C:\Users\Administrator\Documents\Milad\
+cd C:\Users\Administrator\Documents\Milad\Milad-WSP2
 docker build -t milad-wsp2 .
 docker run -d -p 2222:4000 --name milad-wsp2 milad-wsp2
 pause
